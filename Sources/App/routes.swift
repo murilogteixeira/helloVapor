8f5279d26dd50c84d3410d526ca5aed7d0bece54
Murilo Teixeira <murilogteixeira@gmail.com>
2019-10-21 14:30:42 -0300

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,15 @@
 import Vapor
 
+struct LoginRequest: Content {
+    let email: String
+    let senha: String
+}
+
+struct User: Content {
+    let nome: String
+    let email: String
+}
+
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
@@ -10,6 +20,33 @@ public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world!"
+    }
+    
+    router.get("murilo") { req in
+        return "Murilo"
+    }
+    
+    router.post("login") { req -> Future<HTTPStatus> in
+        return try req.content.decode(LoginRequest.self).map(to: HTTPStatus.self) { loginRequest in
+            print(loginRequest.email)
+            print(loginRequest.senha)
+            return .ok
+        }
+    }
+    
+    router.get("user") { req -> User in
+        return User(
+            nome: "Murilo Teixeira",
+            email: "murilo@murilo.com"
+        )
+    }
+    
+    router.get("img") { req -> Response in
+        return req.redirect(to: "/img.png")
+    }
+    
+    router.get("site") { req in
+        return req.redirect(to: "/site/index.html")
     }
 
     // Example of configuring a controller
